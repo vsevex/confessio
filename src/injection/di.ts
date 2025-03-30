16c39bdb99fa2b48d7e28eaa8d961e19bc95d8b5
vsevex <vsevex@gmail.com>
2025-03-30 04:36:41 +0400

    add dependency injection container implementation

---
+++ src/injection/di.ts
@@ -0,0 +1,56 @@
+import funcArgs from "./getFunctionArguments";
+
+interface DiContainer {
+  register(name: string, dependency: any): void;
+  factory(name: string, factory: (...args: any[]) => any): void;
+  inject<T>(factory: (...args: any[]) => T): T;
+  get<T>(name: string): T;
+}
+
+/**
+ * DI Container
+ */
+export default (): DiContainer => {
+  const dependencies: Record<string, any> = {};
+  const factories: Record<string, (...args: any[]) => any> = {};
+
+  const diContainer: DiContainer = {
+    /**
+     * Register dependency
+     */
+    register(name: string, dependency: any): void {
+      dependencies[name] = dependency;
+    },
+
+    /**
+     * Register factory
+     */
+    factory(name: string, factory: (...args: any[]) => any): void {
+      factories[name] = factory;
+    },
+
+    /**
+     * Inject dependencies into factory
+     */
+    inject<T>(factory: (...args: any[]) => T): T {
+      const args = funcArgs(factory).map((dependency) => this.get(dependency));
+      return factory(...args);
+    },
+
+    /**
+     * Get dependency
+     */
+    get<T>(name: string): T {
+      if (!dependencies[name]) {
+        const factory = factories[name];
+        dependencies[name] = factory ? this.inject(factory) : undefined;
+        if (dependencies[name] === undefined) {
+          throw new Error(`Cannot find module: ${name}`);
+        }
+      }
+      return dependencies[name] as T;
+    },
+  };
+
+  return diContainer;
+};
