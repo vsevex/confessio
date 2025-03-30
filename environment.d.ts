e14e21266920c0e49e84d4c43c9cb027b667d10e
vsevex <vsevex@gmail.com>
2025-03-30 04:36:18 +0400

    remove GITHUB_AUTH_TOKEN and NODE_ENV from ProcessEnv interface

--- environment.d.ts
+++ environment.d.ts
@@ -1,8 +1,6 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      GITHUB_AUTH_TOKEN: string;
-      NODE_ENV: "development" | "production";
       PORT?: string;
       PWD: string;
     }
