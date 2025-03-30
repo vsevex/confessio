53ae3a68ab64cf33e47a72aa2bcf91403e405cfc
vsevex <vsevex@gmail.com>
2025-03-31 01:32:18 +0400

    update help command to provide detailed bot information and usage instructions

--- src/bot/commands/help.ts
+++ src/bot/commands/help.ts
@@ -1,10 +1,22 @@
-import { Context, Middleware } from "telegraf";
-import { Update } from "telegraf/types";
+import { Context } from "telegraf";
+import { fmt } from "telegraf/format";
 
 /**
  * Help command controller
  */
-const helpCommand: Middleware<Context> = (ctx: Context<Update>) =>
-  ctx.reply("Help");
+const helpCommand = async (ctx: Context) =>
+  await ctx.reply(fmt`
+Welcome to Confessio – the ultimate anonymous confession bot.
 
-export default { helpCommand };
+Share your secrets, cleanse your soul, or delete your past... for a price.
+
+📝 Commands:
+/info - Get information about the bot.
+/submit – Submit an anonymous confession (requires payment).
+/delete – Remove your confession(s) (requires payment).
+/help – Show this help menu.
+
+Need support? Contact: @Confessio
+`);
+
+export default helpCommand;
