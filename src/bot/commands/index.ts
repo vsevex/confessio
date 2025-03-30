1dea7dd7cabb20ae186bfda51cd5a534bd7b1843
vsevex <vsevex@gmail.com>
2025-03-31 01:32:30 +0400

    refactor command handling to include info command and regex patterns for help and info

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -1,4 +1,15 @@
-import help from "./help";
+import helpCommand from "./help";
+import infoCommand from "./info";
 
-const handlers = new Map();
-handlers.set("help", help);
+const helpRegex = /^(whelp|help)$/i;
+const infoRegex = /^(info|about)$/i;
+
+const myCommands = [
+  { command: "info", description: "Get information about the bot" },
+  {
+    command: "help",
+    description: "Show help menu",
+  },
+];
+
+export default { helpRegex, helpCommand, infoRegex, infoCommand, myCommands };
