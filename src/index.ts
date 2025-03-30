65f6e5c77c0d50eb538636d530e9532eb64bfacf
vsevex <vsevex@gmail.com>
2025-03-30 04:37:13 +0400

    refactor main application structure with dependency injection and error handling

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,77 @@
-import { Telegraf } from "telegraf";
 import * as dotenv from "dotenv";
 
-function main() {
-  // Load environment variables from .env file
-  dotenv.config();
+// Load environment variables from .env file
+dotenv.config();
 
-  const bot = new Telegraf(process.env.BOT_TOKEN!);
+import { Context, Telegraf } from "telegraf";
+import * as winston from "winston";
 
-  bot.launch();
+import config from "./bot/config";
 
-  // Enable graceful stop
-  process.once("SIGINT", () => bot.stop("SIGINT"));
-  process.once("SIGTERM", () => bot.stop("SIGTERM"));
+import LogService from "./log";
+import DiContainer from "./injection";
+import SentryService from "./sentry";
+import bot from "./bot";
+import { Update } from "telegraf/types";
+
+const di = DiContainer();
+
+async function main() {
+  di.register("config", config);
+  di.register("logService", LogService("info", {}));
+  di.register("sentryUrl", config.SENTRY_URL);
+  di.register(
+    "sentryService",
+    new SentryService(config.SENTRY_URL!, {
+      nodeEnv: config.NODE_ENV,
+      sentryTracesSampleRate: "1.0",
+    })
+  );
+
+  di.factory(
+    "bot",
+    async (logService, sentryService) =>
+      await bot(
+        {
+          information: {
+            token: config.botInfo.token,
+            username: config.botInfo.username!,
+          },
+          botDropPendingUpdates: config.BOT_DROP_PENDING_UPDATES,
+        },
+        logService,
+        sentryService
+      )
+  );
+
+  di.get<Telegraf<Context<Update>>>("bot");
 }
 
 main();
+
+const logger = di.get<winston.Logger>("logService");
+const sentry = di.get<SentryService>("sentryService");
+
+async function stopApplication(code?: number | undefined) {
+  const bot = di.get<Telegraf<Context<Update>>>("bot");
+  logger.info("Stopping application...");
+  await Promise.allSettled([
+    sentry.close().then(() => logger.info("Sentry closed")),
+    bot.stop(),
+    logger.info("Bot stopped"),
+  ]);
+  logger.info("Application stopped");
+  process.exit(code);
+}
+
+async function processError(error: Error) {
+  logger.error(error.stack);
+  sentry.captureException(error);
+  await stopApplication(1);
+}
+
+process
+  .once("uncaughtException", processError)
+  .once("unhandledRejection", processError)
+  .once("SIGTERM", () => stopApplication())
+  .once("SIGINT", () => stopApplication());
