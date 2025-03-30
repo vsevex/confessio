8f46a31d0eebd8319f63ea2baf81f0b8d0540cab
vsevex <vsevex@gmail.com>
2025-03-31 01:32:26 +0400

    enhance unmatched message handler to check for recognized commands before replying

--- src/bot/handlers/unmatched.ts
+++ src/bot/handlers/unmatched.ts
@@ -1,8 +1,16 @@
 import { Composer } from "telegraf";
+import commands from "../commands";
 
 export default Composer.on(
   "message",
-  Composer.privateChat((ctx) =>
-    ctx.reply("There was a misunderstanding, do you need /help?")
-  )
+  Composer.privateChat((ctx, next) => {
+    const text = ctx.text;
+    const handled = commands.myCommands.map((cmd) => `/${cmd.command}`);
+
+    if (handled.includes(text ?? "")) {
+      return next();
+    }
+
+    return ctx.reply("There was a misunderstanding, do you need /help?");
+  })
 );
