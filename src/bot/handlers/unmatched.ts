b5fe0b6dcfb13ec3d636c7887bf0f5ed0bb3d676
vsevex <vsevex@gmail.com>
2025-03-31 18:01:04 +0400

    refactor unmatched handler to use SessionContext and improve readability

--- src/bot/handlers/unmatched.ts
+++ src/bot/handlers/unmatched.ts
@@ -1,16 +1,17 @@
-import { Composer } from "telegraf";
 import commands from "../commands";
+import { SessionContext } from "../../session";
 
-export default Composer.on(
-  "message",
-  Composer.privateChat((ctx, next) => {
-    const text = ctx.text;
-    const handled = commands.myCommands.map((cmd) => `/${cmd.command}`);
+export default function unmatchedHandler(
+  ctx: SessionContext,
+  next: () => Promise<void>
+) {
+  const text = ctx.text;
+  const handled = commands.myCommands.map((cmd) => `/${cmd.command}`);
+  handled.push("/start");
 
-    if (handled.includes(text ?? "")) {
-      return next();
-    }
+  if (handled.includes(text ?? "")) {
+    return next();
+  }
 
-    return ctx.reply("There was a misunderstanding, do you need /help?");
-  })
-);
+  return ctx.reply("There was a misunderstanding, do you need /help?");
+}
