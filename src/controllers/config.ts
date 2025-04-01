4b67f7d8077929e9549f4f06d04374ea45994c88
vsevex <vsevex@gmail.com>
2025-04-01 19:00:01 +0400

    add Firestore initialization and database export in config controller

---
+++ src/controllers/config.ts
@@ -0,0 +1,16 @@
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+const serviceAccount = require("../../confessiotg.json");
+
+var db: FirebaseFirestore.Firestore;
+
+function initialize() {
+  initializeApp({
+    credential: cert(serviceAccount),
+  });
+
+  db = getFirestore();
+}
+
+export { initialize, db };
