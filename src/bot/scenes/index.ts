c77fb8275923cb7909bca8a0f436e57d1f44e3b6
vsevex <vsevex@gmail.com>
2025-04-01 19:00:04 +0400

    add scene stage for submission handling in bot

---
+++ src/bot/scenes/index.ts
@@ -0,0 +1,8 @@
+import { Scenes } from "telegraf";
+
+import scenes from "./submissionScene";
+import { SessionContext } from "../../session";
+
+export const stage = new Scenes.Stage<SessionContext>([...scenes], {
+  ttl: 10,
+});
