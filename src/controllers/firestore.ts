e85b954e4263156426ff337e00e6abef8f102fbb
vsevex <vsevex@gmail.com>
2025-04-01 18:59:57 +0400

    add FirestoreController for managing confession submissions

---
+++ src/controllers/firestore.ts
@@ -0,0 +1,39 @@
+import { initialize, db } from "./config";
+import { Confession } from "../models/confession";
+import { Timestamp } from "firebase-admin/firestore";
+import { v4 as uuidv4 } from "uuid";
+
+import * as winston from "winston";
+
+class FirestoreController {
+  private logger: winston.Logger;
+
+  constructor(logger: winston.Logger) {
+    this.logger = logger;
+    logger.warn("Firebase Firestore initialized");
+    initialize();
+  }
+
+  async createConfession(data: Confession): Promise<string> {
+    try {
+      const confessionId = uuidv4();
+      const docRef = await db.collection("confessions").add({
+        ...data,
+        id: confessionId,
+        createdAt: Timestamp.fromDate(data.created_at),
+      });
+      this.logger.info(
+        "Confession created with document ID: ",
+        docRef.id,
+        " and confession ID: ",
+        confessionId
+      );
+      return docRef.id;
+    } catch (error) {
+      this.logger.error("Error creating confession:", error);
+      throw error;
+    }
+  }
+}
+
+export default FirestoreController;
