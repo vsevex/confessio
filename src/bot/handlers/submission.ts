5bc9dd2e268d067b6294e3c04b434f843ae01d32
vsevex <vsevex@gmail.com>
2025-04-02 18:21:07 +0400

    add task management for confession submissions

--- src/bot/handlers/submission.ts
+++ src/bot/handlers/submission.ts
@@ -2,6 +2,7 @@ import FirestoreController from "../../controllers/firestore";
 import di from "../../injection";
 import * as winston from "winston";
 import { SessionContext } from "../../session";
+import TaskController from "../../controllers/task";
 
 export default async function submissionHandler(
   ctx: SessionContext,
@@ -12,16 +13,22 @@ export default async function submissionHandler(
   const firestore: FirestoreController = di().get<FirestoreController>(
     "firestoreController"
   );
+  const task: TaskController = di().get<TaskController>("taskController");
   const user = await ctx.getChat();
 
   try {
     const confessionId = await firestore.createConfession({
+      ref_id: undefined,
       user_id: user.id,
       text: text!,
+      is_published: false,
       is_anonymous: isAnonymous,
       is_deleted: false,
       created_at: new Date(),
     });
+
+    await task.addTask(user.id, confessionId);
+
     logger.info(`Confession submitted with ID: ${confessionId}`);
   } catch (error) {
     // pass
