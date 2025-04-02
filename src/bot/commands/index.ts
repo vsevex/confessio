c8edf1232bff3370f5f2986ec7674c6ac2d63da0
vsevex <vsevex@gmail.com>
2025-04-02 18:21:25 +0400

    add myconfessions command to view, manage, or delete past confessions

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -5,6 +5,7 @@ import infoCommand from "./info";
 const submitRegex = /^(submit|confess)$/i;
 const helpRegex = /^(whelp|help)$/i;
 const infoRegex = /^(info|about)$/i;
+const myConfessionsRegex = /^(myconfessions|myconfessions)$/i;
 
 const myCommands = [
   { command: "info", description: "Get information about the bot" },
@@ -13,6 +14,10 @@ const myCommands = [
     description: "Show help menu",
   },
   { command: "submit", description: "Submit a confession" },
+  {
+    command: "myconfessions",
+    description: "View, manage, or delete your past confessions",
+  },
 ];
 
 export default {
@@ -23,4 +28,5 @@ export default {
   infoCommand,
   myCommands,
   submitRegex,
+  myConfessionsRegex,
 };
