4ebb53dab4c594409e748480789ffe03be3dba8e
vsevex <vsevex@gmail.com>
2025-04-02 18:21:17 +0400

    add ref_id and is_published fields to Confession interface

--- src/models/confession.ts
+++ src/models/confession.ts
@@ -1,6 +1,8 @@
 export interface Confession {
+  ref_id: string | undefined;
   user_id: number | undefined;
   text: string;
+  is_published: boolean;
   is_anonymous: boolean;
   is_deleted: boolean;
   created_at: Date;
