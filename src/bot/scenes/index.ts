e20bad2cc30f508d7500eb7a59e02620a522f6aa
vsevex <vsevex@gmail.com>
2025-04-02 18:21:22 +0400

    increase ttl for scene stage from 10 to 600 seconds

--- src/bot/scenes/index.ts
+++ src/bot/scenes/index.ts
@@ -4,5 +4,5 @@ import scenes from "./submissionScene";
 import { SessionContext } from "../../session";
 
 export const stage = new Scenes.Stage<SessionContext>([...scenes], {
-  ttl: 10,
+  ttl: 600,
 });
